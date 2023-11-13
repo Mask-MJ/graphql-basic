e660321b3106b0effb738774fc70542a6d4900bf
mask <momo617473294@gmail.com>
2023-11-13 08:32:23 +0800

    feat: init

---
+++ src/coffees/coffees.resolver.ts
@@ -0,0 +1,40 @@
+import { Args, ID, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { Coffee } from './entities/coffee.entity';
+import { ParseIntPipe } from '@nestjs/common';
+import { CreateCoffeeInput } from './dto/create-coffee.input';
+import { CoffeesService } from './coffees.service';
+
+@Resolver()
+export class CoffeesResolver {
+  constructor(private readonly coffeesService: CoffeesService) {}
+
+  @Query(() => [Coffee], { name: 'coffees' })
+  async findAll() {
+    return this.coffeesService.findAll();
+  }
+
+  @Query(() => Coffee, { name: 'coffee' })
+  async findOne(@Args('id', { type: () => ID }, ParseIntPipe) id: number) {
+    return this.coffeesService.findOne(id);
+  }
+
+  @Mutation(() => Coffee, { name: 'createCoffee' })
+  async create(
+    @Args('createCoffeeInput') createCoffeeInput: CreateCoffeeInput,
+  ) {
+    return this.coffeesService.create(createCoffeeInput);
+  }
+
+  @Mutation(() => Coffee, { name: 'updateCoffee' })
+  async update(
+    @Args('id', ParseIntPipe) id: number,
+    @Args('updateCoffeeInput') updateCoffeeInput: CreateCoffeeInput,
+  ) {
+    return this.coffeesService.update(id, updateCoffeeInput);
+  }
+
+  @Mutation(() => Coffee, { name: 'removeCoffee' })
+  async remove(@Args('id', ParseIntPipe) id: number) {
+    return this.coffeesService.remove(id);
+  }
+}
---
+++ src/coffees/coffees.service.ts
@@ -0,0 +1,51 @@
+import { Injectable } from '@nestjs/common';
+import { CreateCoffeeInput } from './dto/create-coffee.input';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Coffee } from './entities/coffee.entity';
+import { Repository } from 'typeorm';
+import { UserInputError } from '@nestjs/apollo';
+import { UpdateCoffeeInput } from './dto/update-coffee.input';
+
+@Injectable()
+export class CoffeesService {
+  constructor(
+    @InjectRepository(Coffee)
+    private readonly coffeesRepository: Repository<Coffee>,
+  ) {}
+
+  async findAll() {
+    return this.coffeesRepository.find();
+  }
+
+  async findOne(id: number) {
+    const coffee = await this.coffeesRepository.findOne({ where: { id } });
+
+    if (!coffee) {
+      throw new UserInputError(`Coffee #${id} not found`);
+    }
+    return coffee;
+  }
+
+  async create(createCoffeeInput: CreateCoffeeInput) {
+    const coffee = this.coffeesRepository.create(createCoffeeInput);
+    return this.coffeesRepository.save(coffee);
+  }
+
+  async update(id: number, updateCoffeeInput: UpdateCoffeeInput) {
+    const coffee = await this.coffeesRepository.preload({
+      id,
+      ...updateCoffeeInput,
+    });
+
+    if (!coffee) {
+      throw new UserInputError(`Coffee #${id} not found`);
+    }
+
+    return this.coffeesRepository.save(coffee);
+  }
+
+  async remove(id: number) {
+    const coffee = await this.findOne(id);
+    return this.coffeesRepository.remove(coffee);
+  }
+}
